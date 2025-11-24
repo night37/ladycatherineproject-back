b3af068ff6573b219ac4467b6ca8148209394f76
Jacques-Selaire31 <serra.victor@yahoo.fr>
2025-11-24 11:58:47 +0100

    Ajout de la classe tags

---
+++ App/Entity/Tags.php
@@ -0,0 +1,57 @@
+<?php
+namespace App\Entity;
+use App\Entity\Entity;
+
+class Tags extends Entity {
+    private int $id_tags;
+    private string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+
+    /**
+     * Get the value of id_tags
+     *
+     * @return int
+     */
+    public function getIdTags(): int {
+        return $this->id_tags;
+    }
+
+    /**
+     * Set the value of id_tags
+     *
+     * @param int $id_tags
+     *
+     * @return self
+     */
+    public function setIdTags(int $id_tags): self {
+        $this->id_tags = $id_tags;
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     *
+     * @return string
+     */
+    public function getName(): string {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param string $name
+     *
+     * @return self
+     */
+    public function setName(string $name): self {
+        $this->name = $name;
+        return $this;
+    }
+}
+?>
