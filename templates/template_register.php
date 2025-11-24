7a9bf63af64a224d404a2d0de5832d5d42dc70f6
Guillaume <garnier.guillaume37550@gmail.com>
2025-11-24 16:57:25 +0100

    login ok

---
+++ templates/template_register.php
@@ -0,0 +1,28 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+    <title><?= $title ?? "" ?></title>
+</head>
+<body>
+    <main class="container">
+    <div>
+        <h1>Ajouter un compte</h1>
+        <form action="" method="post">
+            <input type="text" name="firstname" id="" placeholder="saisir votre prénom">
+            <input type="text" name="lastname" id="" placeholder="saisir votre nom">
+            <input type="email" name="email" id="" placeholder="saisir votre email">
+            <input type="password" name="password" id="" placeholder="saisir votre mot de passe">
+            <input type="password" name="verif_password" id="" placeholder="confirmer votre mot de passe">
+            <input type="submit" value="Ajouter" name="submit">
+        </form>
+        <p><?= $data["message"] ?? "" ?></p>
+    </div>
+    </main>
+</body>
+
+</html>
